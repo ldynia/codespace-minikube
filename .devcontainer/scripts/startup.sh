a3db7cad7aca8cf8ecf3442b714b6272de2cce1e
ldynia <l_dynowski@yahoo.com>
2023-03-01 13:30:17 +0100

    Update startup.sh

---
+++ .devcontainer/scripts/startup.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+function start_minikube() {
+  echo "Start minikube"
+  minikube start --wait "apiserver,system_pods,node_ready,kubelet"
+  sudo -- bash -c "echo '$(minikube ip) minikube' >> /etc/hosts"
+}
+
+function start_github_runner() {
+  echo "Setup github runner folders"
+  GITHUB_ACTION_DIR=/usr/src/actions-runner
+  sudo chown $USER:$USER /usr/src
+  mkdir -p $GITHUB_ACTION_DIR
+
+  cd $GITHUB_ACTION_DIR
+  GITHUB_RUNNER_VERSION=2.301.1
+  if [ ! -f $GITHUB_ACTION_DIR/actions-runner-linux-x64-$GITHUB_RUNNER_VERSION.tar.gz ]; then
+      echo "Downloading Runner And Install Dependencies"
+      curl -o actions-runner-linux-x64-$GITHUB_RUNNER_VERSION.tar.gz -L https://github.com/actions/runner/releases/download/v$GITHUB_RUNNER_VERSION/actions-runner-linux-x64-$GITHUB_RUNNER_VERSION.tar.gz
+      tar xzf ./actions-runner-linux-x64-$GITHUB_RUNNER_VERSION.tar.gz
+  fi
+
+  # https://docs.github.com/en/rest/actions/self-hosted-runners?apiVersion=2022-11-28#create-a-registration-token-for-an-organization
+  echo "Obtaining codespaces secrets PAT"
+  GITHUB_RUNNER_PAT=$(curl -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $PAT" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/$GITHUB_REPOSITORY/actions/runners/registration-token | jq --raw-output .token);
+
+  echo "Remove github runner"
+  $GITHUB_ACTION_DIR/config.sh remove --token $GITHUB_RUNNER_PAT
+
+  echo "Configure github runner"
+  $GITHUB_ACTION_DIR/config.sh \
+    --labels "self-hosted,Linux,X64,$RepositoryName" \
+    --name $RepositoryName \
+    --replace \
+    --token $GITHUB_RUNNER_PAT \
+    --unattended \
+    --url https://github.com/$GITHUB_REPOSITORY \
+    --work $GITHUB_ACTION_DIR/_work
+
+  echo "Start github runner in foreground"
+  $GITHUB_ACTION_DIR/run.sh > _diag/runner.log
+}
+
+# Execute functions
+start_minikube
+start_github_runner
---
+++ README.md
@@ -0,0 +1,39 @@
+# Setup
+
+1. In [Settings > Developer settings](https://github.com/settings/tokens/new)
+   - create `personal access token (classic)` with `repo` scope
+1. In [Settings > Codespaces](https://github.com/settings/codespaces/secrets/new)
+   - create `Codespaces secrets` named `PAT`
+   - bind it to this repository
+1. In repository [Settings > Actions > Runners](https://github.com/ldynia/codespace-minikube/settings/actions/runners)
+   - Verify that self-hosted runner is in `Active` or `Idle` state
+
+# Minikube
+
+```bash
+kubectl apply -f devops/k8s/manifests
+kubectl port-forward service/nginx 8080:80
+```
+
+# TODO
+
+[start a background server process in a job github self hosted runner](https://stackoverflow.com/questions/68439803/self-hosted-github-runner-start-a-background-server-process-in-a-job-and-let-it)
+
+# Troubleshoot Self Hosted Runner
+
+1. Test Connectivity
+
+    ```bash
+    GH_ACTION_DIR=/usr/src/actions-runner
+
+    SELF_HOSTED_RUNNER_PAT_TOKEN=$(curl -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $PAT_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/$GITHUB_REPOSITORY/actions/runners/registration-token | jq --raw-output .token)
+
+    $GH_ACTION_DIR/run.sh --check --url https://github.com/$GITHUB_REPOSITORY --pat $SELF_HOSTED_RUNNER_PAT_TOKEN
+    ```
+
+1. Remove Runner
+
+    ```bash
+    $GH_ACTION_DIR/config.sh remove --token $SELF_HOSTED_RUNNER_PAT_TOKEN
+    ```
+
